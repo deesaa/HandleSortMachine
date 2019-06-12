2f44d77567f38ba503596d40806b447ce79459a2
deesaa <deemsaer@gmail.com>
2019-06-12 15:37:45 +0300

    1

--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { BrowserWindow } = require('electron').remote;
+const { dialog } = require('electron').remote;
+
+const chooseFTS = document.getElementById('choose-folder-to-sort');
+
+chooseFTS.addEventListener('click', (e) => {
+    let folderPath = dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
+
+    if (folderPath !== undefined) {
+        document.getElementById('folder-to-sort').value = folderPath;
+    } else {
+        console.log('Denied');
+    }
+});
